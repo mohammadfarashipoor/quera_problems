3b8dfee00ec0ed2fd96d64ff1066a7be15b00041
Mohammad <mohammad1f2f@gmail.com>
2023-10-31 18:54:21 +0330

    feat: done solve problem 134355

--- context-creator/src/lib/createGlobalState.js
+++ context-creator/src/lib/createGlobalState.js
@@ -1,5 +1,41 @@
-import { createContext, useContext, useState } from "react";
+import {createContext, useContext, useEffect, useState} from "react";
 
 export const createGlobalState = (instructions) => {
-  // YOUR CODE HERE
-};
+  const ComponentsContext = createContext();
+  let stateName = ""
+
+
+  function destructurState(object){
+    for (const stateKey in object) {
+      if(typeof object[stateKey] !== "function"){
+        stateName = stateKey
+      }
+    }
+  }
+  destructurState(instructions())
+
+  const ComponentsProvider = ({children}) => {
+    const [setter, setSetter] = useState(instructions()[stateName]);
+    const [state, setState] = useState(instructions(setSetter));
+
+    useEffect(() => {
+      setState({...state,[stateName]:setter})
+    }, [setter]);
+
+    return (
+      <ComponentsContext.Provider value={state}>
+        {children}
+      </ComponentsContext.Provider>
+    );
+  };
+
+  const useGlobalState = () => {
+    const context = useContext(ComponentsContext);
+    if (context === undefined) {
+      throw new Error(`${stateName} must be used within a StateProvider`);
+    }
+    return context;
+  };
+
+  return [ComponentsProvider, useGlobalState];
+}
